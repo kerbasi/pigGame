e1eec8ea20a6bdcee9b0e38ac462cee39a0b907e
Kirill <kirill.novikov@gmail.com>
2023-03-16 14:59:47 +0300

    Link script and add random func

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+const diceEl = document.querySelector('.dice');
+const rollBtn = document.querySelector('.btn_type_roll');
+
+const getRandomDice = () => {
+  const randomNum = Math.ceil(Math.random() * 6);
+  diceEl.src = `dice-${randomNum}.png`;
+};
+
+rollBtn.addEventListener('click', getRandomDice);
