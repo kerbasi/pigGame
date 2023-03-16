a67a90c69b410664edf05d63e62710e42593f52b
Kirill <kirill.novikov@gmail.com>
2023-03-16 15:27:10 +0300

    Add hold and reset handlers

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 'use strict';
 const diceEl = document.querySelector('.dice');
 const rollBtn = document.querySelector('.btn_type_roll');
+const holdBtn = document.querySelector('.btn_type_hold');
+const newGameBtn = document.querySelector('.btn_type_new-game');
 const playerCards = document.querySelectorAll('.player-card');
 const player1CurrentEl = playerCards[0].querySelector(
   '.player-card__current-score'
 );
+const player1TotalEl = playerCards[0].querySelector(
+  '.player-card__total-score'
+);
 const player2CurrentEl = playerCards[1].querySelector(
   '.player-card__current-score'
+);
+const player2TotalEl = playerCards[1].querySelector(
+  '.player-card__total-score'
 );
 
 let isFirstPlayer = true;
@@ -52,5 +60,32 @@ rollBtn.addEventListener('click', () => {
       setText(player2CurrentEl, player2Current);
       switchPlayer();
     }
+  }
+});
+
+holdBtn.addEventListener('click', () => {
+  if (isFirstPlayer) {
+    player1Total += player1Current;
+    setText(player1CurrentEl, 0);
+    setText(player1TotalEl, player1Total);
+  } else {
+    player2Total += player2Current;
+    setText(player2CurrentEl, 0);
+    setText(player2TotalEl, player2Total);
+  }
+  switchPlayer();
+});
+
+newGameBtn.addEventListener('click', () => {
+  player1Current = 0;
+  player1Total = 0;
+  player2Current = 0;
+  player2Total = 0;
+  setText(player1CurrentEl, player1Current);
+  setText(player1TotalEl, player1Total);
+  setText(player2CurrentEl, player2Current);
+  setText(player2TotalEl, player2Total);
+  if (!isFirstPlayer) {
+    switchPlayer();
   }
 });
